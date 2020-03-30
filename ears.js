6d68d4993463a70ec702aa0cfdf7e30a8e4f5269
healzer <justdoitzlol@gmail.com>
2020-03-30 14:05:49 +0000

    inital commit

---
+++ ears.js
@@ -0,0 +1,330 @@
+//////////////////////////////////////////
+//////////////// LOGGING /////////////////
+//////////////////////////////////////////
+function getCurrentDateString() {
+    return (new Date()).toISOString() + ' ::';
+};
+__originalLog = console.log;
+console.log = function () {
+    var args = [].slice.call(arguments);
+    __originalLog.apply(console.log, [getCurrentDateString()].concat(args));
+};
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+
+//////////////////////////////////////////
+///////////////// VARIA //////////////////
+//////////////////////////////////////////
+
+function necessary_dirs() {
+    if (!fs.existsSync('./temp/')){
+        fs.mkdirSync('./temp/');
+    }
+    if (!fs.existsSync('./data/')){
+        fs.mkdirSync('./data/');
+    }
+}
+necessary_dirs()
+
+
+function clean_temp() {
+    const dd = './temp/';
+    fs.readdir(dd, (err, files) => {
+        if (err) throw err;
+
+        for (const file of files) {
+            fs.unlink(path.join(dd, file), err => {
+                if (err) throw err;
+            });
+        }
+    });
+}
+clean_temp(); // clean files at startup
+
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
+
+
+async function convert_audio(infile, outfile, cb) {
+    try {
+        let SoxCommand = require('sox-audio');
+        let command = SoxCommand();
+        streamin = fs.createReadStream(infile);
+        streamout = fs.createWriteStream(outfile);
+        command.input(streamin)
+            .inputSampleRate(48000)
+            .inputEncoding('signed')
+            .inputBits(16)
+            .inputChannels(2)
+            .inputFileType('raw')
+            .output(streamout)
+            .outputSampleRate(16000)
+            .outputEncoding('signed')
+            .outputBits(16)
+            .outputChannels(1)
+            .outputFileType('wav');
+
+        command.on('end', function() {
+            streamout.close();
+            streamin.close();
+            cb();
+        });
+        command.on('error', function(err, stdout, stderr) {
+            console.log('Cannot process audio: ' + err.message);
+            console.log('Sox Command Stdout: ', stdout);
+            console.log('Sox Command Stderr: ', stderr)
+        });
+
+        command.run();
+    } catch (e) {
+        console.log('convert_audio: ' + e)
+    }
+}
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+
+//////////////////////////////////////////
+//////////////// CONFIG //////////////////
+//////////////////////////////////////////
+
+const SETTINGS_FILE = 'settings.json';
+
+let DISCORD_TOK = null;
+let witAPIKEY = null; 
+let SPOTIFY_TOKEN_ID = null;
+let SPOTIFY_TOKEN_SECRET = null;
+
+function loadConfig() {
+    const CFG_DATA = JSON.parse( fs.readFileSync(SETTINGS_FILE, 'utf8') );
+    
+    DISCORD_TOK = CFG_DATA.discord_token;
+    witAPIKEY = CFG_DATA.wit_ai_token;
+}
+loadConfig()
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
+
+
+const Discord = require('discord.js')
+const DISCORD_MSG_LIMIT = 2000;
+const discordClient = new Discord.Client()
+discordClient.on('ready', () => {
+    console.log(`Logged in as ${discordClient.user.tag}!`)
+})
+discordClient.login(DISCORD_TOK)
+
+const PREFIX = '*';
+const _CMD_HELP        = PREFIX + 'help';
+const _CMD_JOIN        = PREFIX + 'join';
+const _CMD_LEAVE       = PREFIX + 'leave';
+const _CMD_DEBUG       = PREFIX + 'debug';
+const _CMD_TEST        = PREFIX + 'hello';
+
+const guildMap = new Map();
+
+
+discordClient.on('message', async (msg) => {
+    try {
+        if (!('guild' in msg) || !msg.guild) return; // prevent private messages to bot
+        const mapKey = msg.guild.id;
+        if (msg.content.trim().toLowerCase() == _CMD_JOIN) {
+            if (!guildMap.has(mapKey))
+                await connect(msg, mapKey)
+            else
+                msg.reply('Already connected')
+        } else if (msg.content.trim().toLowerCase() == _CMD_LEAVE) {
+            if (guildMap.has(mapKey)) {
+                let val = guildMap.get(mapKey);
+                if (val.voice_Channel) val.voice_Channel.leave()
+                if (val.voice_Connection) val.voice_Connection.disconnect()
+                if (val.musicYTStream) val.musicYTStream.destroy()
+                    guildMap.delete(mapKey)
+                msg.reply("Disconnected.")
+            } else {
+                msg.reply("Cannot leave because not connected.")
+            }
+        } else if (msg.content.trim().toLowerCase() == _CMD_HELP) {
+            msg.reply(getHelpString());
+        }
+        else if (msg.content.trim().toLowerCase() == _CMD_DEBUG) {
+            console.log('toggling debug mode')
+            let val = guildMap.get(mapKey);
+            if (val.debug)
+                val.debug = false;
+            else
+                val.debug = true;
+        }
+        else if (msg.content.trim().toLowerCase() == _CMD_TEST) {
+            msg.reply('hello back =)')
+        }
+    } catch (e) {
+        console.log('discordClient message: ' + e)
+        msg.reply('Error#180: Something went wrong, try again or contact the developers if this keeps happening.');
+    }
+})
+
+function getHelpString() {
+    let out = '**COMMANDS:**\n'
+        out += PREFIX + 'join\n';
+        out += PREFIX + 'leave\n';
+    return out;
+}
+
+
+async function connect(msg, mapKey) {
+    try {
+        let voice_Channel = await discordClient.channels.fetch(msg.member.voice.channelID);
+        if (!voice_Channel) return msg.reply("Error: The voice channel does not exist!");
+        let text_Channel = await discordClient.channels.fetch(msg.channel.id);
+        if (!text_Channel) return msg.reply("Error: The text channel does not exist!");
+        let voice_Connection = await voice_Channel.join();
+        voice_Connection.play('sound.mp3', { volume: 0.5 });
+        guildMap.set(mapKey, {
+            'text_Channel': text_Channel,
+            'voice_Channel': voice_Channel,
+            'voice_Connection': voice_Connection,
+            'musicQueue': [],
+            'musicDispatcher': null,
+            'musicYTStream': null,
+            'currentPlayingTitle': null,
+            'currentPlayingQuery': null,
+            'debug': false,
+        });
+        speak_impl(voice_Connection, mapKey)
+        voice_Connection.on('disconnect', async(e) => {
+            if (e) console.log(e);
+            guildMap.delete(mapKey);
+        })
+        msg.reply('connected!')
+    } catch (e) {
+        console.log('connect: ' + e)
+        msg.reply('Error: unable to join your voice channel.');
+        throw e;
+    }
+}
+
+
+function speak_impl(voice_Connection, mapKey) {
+    voice_Connection.on('speaking', async (user, speaking) => {
+        if (speaking.bitfield == 0 /*|| user.bot*/) {
+            return
+        }
+        console.log(`I'm listening to ${user.username}`)
+
+        const filename = './temp/audio_' + mapKey + '_' + user.username.replace(/[^a-z0-9]/gi, '_').toLowerCase() + '_' + Date.now() + '.tmp';
+        let ws = fs.createWriteStream(filename);
+
+        // this creates a 16-bit signed PCM, stereo 48KHz stream
+        const audioStream = voice_Connection.receiver.createStream(user, { mode: 'pcm' })
+        audioStream.pipe(ws)
+
+        audioStream.on('error',  (e) => { 
+            console.log('audioStream: ' + e)
+        });
+        ws.on('error',  (e) => { 
+            console.log('ws error: ' + e)
+        });
+        audioStream.on('end', async () => {
+            const stats = fs.statSync(filename);
+            const fileSizeInBytes = stats.size;
+            const duration = fileSizeInBytes / 48000 / 4;
+            console.log("duration: " + duration)
+
+            if (duration < 0.5 || duration > 19) {
+                console.log("TOO SHORT / TOO LONG; SKPPING")
+                fs.unlinkSync(filename)
+                return;
+            }
+
+            const newfilename = filename.replace('.tmp', '.raw');
+            fs.rename(filename, newfilename, (err) => {
+                if (err) {
+                    console.log('ERROR270:' + err)
+                    fs.unlinkSync(filename)
+                } else {
+                    let val = guildMap.get(mapKey)
+                    const infile = newfilename;
+                    const outfile = newfilename + '.wav';
+                    try {
+                        convert_audio(infile, outfile, async () => {
+                            let out = await transcribe_witai(outfile);
+                            if (out != null)
+                                process_commands_query(out, mapKey, user);
+                            if (!val.debug) {
+                                fs.unlinkSync(infile)
+                                fs.unlinkSync(outfile)
+                            }
+                        })
+                    } catch (e) {
+                        console.log('tmpraw rename: ' + e)
+                        if (!val.debug) {
+                            fs.unlinkSync(infile)
+                            fs.unlinkSync(outfile)
+                        }
+                    }
+                }
+
+            });
+
+
+        })
+    })
+}
+
+
+function process_commands_query(txt, mapKey, user) {
+    // console.log(user.name+': '+txt)
+    if (txt && txt.length) {
+        console.log(user)
+        let val = guildMap.get(mapKey);
+        val.text_Channel.send(user.username + ': ' + txt)
+    }
+}
+
+
+//////////////////////////////////////////
+//////////////// SPEECH //////////////////
+//////////////////////////////////////////
+let witAI_lastcallTS = null;
+const witClient = require('node-witai-speech');
+async function transcribe_witai(file) {
+    try {
+        // ensure we do not send more than one request per second
+        if (witAI_lastcallTS != null) {
+            let now = Math.floor(new Date());    
+            while (now - witAI_lastcallTS < 1000) {
+                console.log('sleep')
+                await sleep(1000);
+                now = Math.floor(new Date());
+            }
+        }
+    } catch (e) {
+        console.log('transcribe_witai 837:' + e)
+    }
+
+    try {
+        console.log('transcribe_witai')
+        const extractSpeechIntent = util.promisify(witClient.extractSpeechIntent);
+        var stream = fs.createReadStream(file);
+        const output = await extractSpeechIntent(witAPIKEY, stream, "audio/wav")
+        witAI_lastcallTS = Math.floor(new Date());
+        console.log(output)
+        stream.destroy()
+        if (output && '_text' in output && output._text.length)
+            return output._text
+        return output;
+    } catch (e) { console.log('transcribe_witai 851:' + e) }
+}
+//////////////////////////////////////////
+//////////////////////////////////////////
+//////////////////////////////////////////
